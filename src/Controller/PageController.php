908ca06d55ba24910ec44adebaddbbad608a1099
LUPUS001 <antoniodaw23@gmail.com>
2025-12-16 08:01:40 +0000

    Creando formularios y adaptandolos para Symfony

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -2,9 +2,13 @@
 
 namespace App\Controller;
 
+use App\Form\ContactFormType; //Clase del formulario
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request; //Clase de la petición
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\Persistence\ManagerRegistry;
+use App\Entity\Contact;
 
 class PageController extends AbstractController
 {
@@ -34,9 +38,32 @@ class PageController extends AbstractController
         return $this->render('page/team.html.twig', []);
     }
 
-    #[Route('/contact', name: 'contact')]
+    /* #[Route('/contact', name: 'contact')]
     public function contact(): Response
     {
         return $this->render('page/contact.html.twig', []);
+    } */
+
+    #[Route('/contact', name: 'contact')]
+    public function contact(ManagerRegistry $doctrine, Request $request): Response
+    {
+        $contact = new Contact();
+        $form = $this->createForm(ContactFormType::class, $contact);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $contacto = $form->getData();    
+            $entityManager = $doctrine->getManager();    
+            $entityManager->persist($contacto);
+            $entityManager->flush();
+
+            // Para confirmarle al usuario que se ha enviado el formulario
+            $this->addFlash('success', '¡Gracias por contactarnos! Tu mensaje ha sido enviado correctamente.');
+
+            //Para redigirir al index cuando se envie el formulario
+            return $this->redirectToRoute('index', []);
+        }
+        return $this->render('page/contact.html.twig', array(
+            'form' => $form->createView()    
+        ));
     }
 }
