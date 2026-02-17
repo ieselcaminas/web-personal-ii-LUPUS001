df359df36c0a66fe3d03ad76a548a7b5051e3241
LUPUS001 <antoniodaw23@gmail.com>
2026-02-17 04:01:14 +0000

    5.7 - Implementar funcionalidad de comentarios en los posts y limpieza de migraciones

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -33,15 +33,34 @@ final class BlogController extends AbstractController
     }
 
     #[Route('/single_post/{slug}', name: 'single_post')]
-    public function post(ManagerRegistry $doctrine, $slug): Response
+    public function post(ManagerRegistry $doctrine, Request $request, $slug): Response
     {
-        $repositorio = $doctrine->getRepository(Post::class);
-        $post = $repositorio->findOneBy(["slug"=>$slug]);
-        $recents = $repositorio->findRecents();
+        $repository = $doctrine->getRepository(Post::class);
+        $post = $repository->findOneBy(["slug"=>$slug]);
+        $recents = $repository->findRecents();
+        
+        $comment = new \App\Entity\Comment();
+        $form = $this->createForm(\App\Form\CommentFormType::class, $comment);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $comment = $form->getData();
+            $comment->setPost($post);
+            
+            // Aumentamos en 1 el número de comentarios del post
+            $post->setNumComments($post->getNumComments() + 1);
+
+            $entityManager = $doctrine->getManager();    
+            $entityManager->persist($comment);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('single_post', ["slug" => $post->getSlug()]);
+        }
 
         return $this->render('blog/single_post.html.twig', [
             'post' => $post,
-            'recents' => $recents
+            'recents' => $recents,
+            'commentForm' => $form->createView()
         ]);
     }
     
